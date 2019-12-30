780ac0ecb54aa688d6225d17081b548443972578
Surafel Seyoum <lesurafel@gmail.com>
2019-12-29 21:34:31 -0800

    Add update sub price

--- js/application.js
+++ js/application.js
@@ -1,1 +1,19 @@
+var updateSubPrice = function (ele) {
+  var unitPrice = $(ele).children('.unitPrice').text();
+  unitPrice = parseFloat(unitPrice.slice(1));
+  var qty = parseFloat($(ele).find('.cost input').val());
 
+  if (qty < 0 || !qty) {
+    $(ele).find('.cost input').val('');
+    qty = 0;
+  }
+  var subPrice = unitPrice * qty;
+  if (subPrice) {
+    $(ele).children('.subPrice').html('$' + subPrice + '.00');
+  } else {
+    $(ele).children('.subPrice').html('$--.--');
+    subPrice = 0;
+  }
+
+  return subPrice;
+}
